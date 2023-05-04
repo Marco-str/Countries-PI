3015c0a495bbcbd09f55c32513ab11264fcd70ef
Marco Salazar <122067295+Marco-str@users.noreply.github.com>
2023-05-04 17:21:19 -0300

    Formulario Con validadciones

--- client/src/components/Filtros/FCheckbox.jsx
+++ client/src/components/Filtros/FCheckbox.jsx
@@ -1,77 +1,92 @@
+
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useState } from "react";
 import axios from "axios";
 
 const FCheckbox = () => {
-  const countries = useSelector((state) => state.countries);
 
-  const [selectedContinent, setSelectedContinent] = useState("");
-  const [filteredCountries, setFilteredCountries] = useState([]);
+  /***************************************************** */
 
+
+   const continent = useSelector((state) => state.continent);
+
+   const [selectedContinent, setSelectedContinent] = useState("");
+   const [filteredCountries, setFilteredCountries] = useState([]);
+
+
+   
+   const filterCountries = () => {
+     const filtered = continent.filter(
+       (continent) => continent.continent === selectedContinent
+       );
+       setFilteredCountries(filtered);
+      };
+      
+      
+      
   const changeHandler = (event) => {
-    setSelectedContinent(event.target.value);
-  };
+       setSelectedContinent(event.target.value);
+   };
 
-  const filterCountries = () => {
-    const filtered = countries.filter(
-      (country) => country.continent === selectedContinent
-    );
-    setFilteredCountries(filtered);
-  };
+   const submitHandler = (e) => {
+    e.preventDefault()
+    buscar(search)
+    setSearch('')
+}
 
+
+/***************************************************** */
+
+
+
+  //  const [categories, setCategories] = useState("");
+
+  //  useEffect(  async () => {
+  //   await axios.get("http:localhost:3001/countries")
+  //    .then((response) => response.json())
+  //    .then((data) => setCategories(data));
+  //  }, [categories]);
+  //  console.log(categories);
+
+
+/*********************************** CHECK BOX DE FILTRADO *************************************************** */
   return (
     <>
+
+   
       <div>
-        <input
-          type="checkbox"
-          name="continent"
-          value="Asia"
-          onChange={changeHandler}
-        />
-        <label> Asia</label>
+
+        <input type="checkbox" name="continent" value="Asia" onChange={changeHandler} onClick={submitHandler} />
+          <label> Asia</label>
+
 
         <input
-          type="checkbox"
-          name="continent"
-          value="Africa"
-          onChange={changeHandler}
-        />
-        <label> Africa</label>
+          type="checkbox" name="continent" value="Africa" onChange={changeHandler} onClick={submitHandler} />
+          <label> Africa</label>
 
-        <input
-          type="checkbox"
-          name="continent"
-          value="America"
-          onChange={changeHandler}
-        />
-        <label> America</label>
 
-        <input
-          type="checkbox"
-          name="continent"
-          value="Europe"
-          onChange={changeHandler}
-        />
-        <label> Europe</label>
+        <input type="checkbox" name="continent" value="America" onChange={changeHandler} onClick={submitHandler} />
+          <label> America</label>
 
-        <input
-          type="checkbox"
-          name="continent"
-          value="Oceania"
-          onChange={changeHandler}
-        />
-        <label> Oceania</label>
+        <input  type="checkbox"   name="continent"  value="Europe" onChange={changeHandler} onClick={submitHandler} />
+          <label> Europe</label>
 
-        <input
-          type="checkbox"
-          name="continent"
-          value="Antartida"
-          onChange={changeHandler}
-        />
-        <label> Antartida</label>
+
+        <input  type="checkbox"  name="continent"  value="Oceania" onChange={changeHandler} onClick={submitHandler} />
+         <label> Oceania</label>
+
+
+        <input  type="checkbox"   name="continent"  value="Antartida"   onChange={changeHandler} onClick={submitHandler} />
+          <label> Antartida</label>
+
       </div>
 
-      <button onClick={filterCountries}>Filtrar</button>
+
+               <button onClick={filterCountries}>Filtrar</button>
+
+
+{/* /***************************************************** ***************************************************** */}
+
 
       {filteredCountries.length > 0 ? (
         <div>
@@ -87,6 +102,7 @@ const FCheckbox = () => {
       ) : (
         <p>Selecciona un continente para filtrar los países</p>
       )}
+
     </>
   );
 };
